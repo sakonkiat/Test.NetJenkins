2268f6b614fc63522f1f3c67b4c8b945236dcc03
Sakonkiat <s.sakonkiat@gmail.com>
2018-05-03 17:58:32 +0700

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using Service.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("D:\\testjenkins.log", true, Encoding.UTF8);
+            sw.WriteLine(string.Format("{0} : Service started.",DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+            sw.Close();
+        }
+    }
+}
---
+++ TestJenkins_WinServ/Service1.cs
@@ -0,0 +1,36 @@
+﻿using Service.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJenkins_WinServ
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("D:\\testjenkins.log", true, Encoding.UTF8);
+            sw.WriteLine(string.Format("{0} : Service started.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+            sw.Close();
+        }
+
+        protected override void OnStop()
+        {
+            StreamWriter sw = new StreamWriter("D:\\testjenkins.log", true, Encoding.UTF8);
+            sw.WriteLine(string.Format("{0} : Service Stopped.", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+            sw.Close();
+        }
+    }
+}
